8aec36249f410e4a303fa17de4639f5e9077333e
Shivaraja S <Shivaraja.S@philips.com>
2024-07-21 23:38:19 +0530

    Implement stats functionality

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,22 @@
 #include "stats.h"
+#include<algorithm>
+#include<numeric>
+#include<math.h>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& vec) {
+
+	Statistics::Stats stat{};
+	if (!vec.empty())
+	{
+		stat.min = *std::min_element(vec.begin(), vec.end());
+		stat.max = *std::max_element(vec.begin(), vec.end());
+		stat.average = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+	}
+	else
+	{
+		stat.min = NAN;
+		stat.max = NAN;
+		stat.average = NAN;
+	}
+	return stat;
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,12 @@
 #include <vector>
 
 namespace Statistics {
-    // define the Stats structure here. See the tests to infer its properties
+
+    struct Stats {
+        float average;
+		float max;
+		float min;
+    };
     
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<float>& );
 }
